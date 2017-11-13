80a83a82a71736c2f99d0733052d4632fb296f84
Rajarshi Guha <rajarshi.guha@gmail.com>
2017-11-12 20:03:26 -0500

    Updated to include solubility trends

--- app/controllers/ChemblQueries.scala
+++ app/controllers/ChemblQueries.scala
@@ -11,10 +11,29 @@ import play.api.db.Database
   */
 class ChemblQueries @Inject()(db: Database) extends EntityHelper {
 
+  def medianSolubility(smi: String): Map[Int, Double] = {
+    if (smi.isEmpty) None
+    db.withConnection { conn =>
+      val pst = conn.prepareStatement("select docs.year, median(desc_value) as val " +
+        "from  compound_structures cs, " +
+        "activities act, docs, ste_descriptors sted " +
+        "where rdmol_smiles@>'" + smi + "' " +
+        "and cs.molregno = act.molregno " +
+        "and cs.molregno = sted.molregno " +
+        "and sted.desc_name = 'logS' " +
+        "and act.doc_id = docs.doc_id " +
+        "and docs.year is not null " +
+        "group by year " +
+        "order by year")
+      val riter = results(pst.executeQuery())(rs => Map(rs.getInt(1) -> rs.getDouble(2)))
+      riter.toList.flatten.toMap
+    }
+  }
+
   def medianQED(smi: String): Map[Int, Double] = {
     if (smi.isEmpty) None
     db.withConnection { conn =>
-      val pst = conn.prepareStatement("select docs.year, median(desc_value) as qed " +
+      val pst = conn.prepareStatement("select docs.year, median(desc_value) as val " +
         "from  compound_structures cs, " +
         "activities act, docs, ste_descriptors sted " +
         "where rdmol_smiles@>'" + smi + "' " +
@@ -33,7 +52,7 @@ class ChemblQueries @Inject()(db: Database) extends EntityHelper {
   def compoundCounts(smi: String): Map[Int, Double] = {
     if (smi.isEmpty) None
     db.withConnection { conn =>
-      val pst = conn.prepareStatement("select docs.year, count(cs.molregno) as nmol " +
+      val pst = conn.prepareStatement("select docs.year, count(cs.molregno) as val " +
         "from compound_structures cs, " +
         "activities act, docs " +
         "where rdmol_smiles@>'" + smi + "' " +
@@ -52,7 +71,7 @@ class ChemblQueries @Inject()(db: Database) extends EntityHelper {
     if (smi.isEmpty) None
 
     db.withConnection { conn =>
-      val pst = conn.prepareStatement("select docs.year, count(distinct act.assay_id) as nassay " +
+      val pst = conn.prepareStatement("select docs.year, count(distinct act.assay_id) as val " +
         "from compound_structures cs, activities_robustz act, docs " +
         "where " +
         "rdmol_smiles@>'" + smi + "' " +
